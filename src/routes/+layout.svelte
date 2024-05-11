29d53425e4aa9d30be9dc7e67037a71ca867805a
Syamil Maulod <syamil8@gmail.com>
2024-05-11 17:32:37 +0200

    cleanup layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,11 +17,6 @@
 		"text-white bg-green-700 text-lg md:bg-transparent md:text-green-700 md:dark:text-white dark:bg-green-600 md:dark:bg-transparent";
 	let nonActiveClass =
 		"text-gray-700 text-lg hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-green-700 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent";
-	import Banner from "$lib/components/layout/Banner.svelte";
-
-	import { CloseButton } from "flowbite-svelte";
-	export let data;
-	export let visible = true;
 </script>
 
 <svelte:head>
